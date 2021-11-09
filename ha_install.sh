072ef400d4f71456e37ae5e9472eeb07b3f08fe0
Ivan Belokobylskiy <belokobylskij@gmail.com>
2021-11-09 14:32:59 +0300

    Upgrade to 2021.11.2

--- ha_install.sh
+++ ha_install.sh
@@ -8,7 +8,7 @@ PYTHON_VERSION="3.9"
 if [ "${OPENWRT_VERSION}" == "19.07" ]; then
   PYTHON_VERSION="3.7"
 fi
-HOMEASSISTANT_VERSION="2021.11.1"
+HOMEASSISTANT_VERSION="2021.11.2"
 
 echo "=========================================="
 echo " Installing Home Assistant ${HOMEASSISTANT_VERSION} ..."
